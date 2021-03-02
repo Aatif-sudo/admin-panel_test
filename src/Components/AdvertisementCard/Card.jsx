aad8757c9d1b5e2c47d8df8a179846fc02289be6
aatif_khan <aatifkhan96.ak@gmail.com>
2021-03-02 17:01:36 +0530

    commit

--- src/Components/AdvertisementCard/Card.jsx
+++ src/Components/AdvertisementCard/Card.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Card.scss";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 function Card({ image, video, name, expiry_date, URL }) {
   return (
     <div className="card">
       <h3>{name}</h3>
       <div className="file_container">
-        <Link to={URL}>{image && <img src={image} alt="file" />}</Link>
-        <Link to={URL}>{video && <video src={video} controls />}</Link>
+        <a href={URL} target="_blank">
+          {image && <img src={image} alt="file" />}
+        </a>
+        <a href={URL} target="_blank">
+          {video && <video src={video} controls />}
+        </a>
       </div>
       <h6 className="date">
         Advertisement Expiry on - {expiry_date?.toLocaleString()}
